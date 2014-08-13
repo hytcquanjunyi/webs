422d652f7450224a865c9bdfd932249f2e4eb1f9
hytcquanjunyi <445218372@qq.com>
2014-08-13 16:54:01 +0800

    2014-8-13百度注册登录
    基本完成注册登录功能，还有一些功能细节有待实现

--- baiduSignInSignUp/js/index.js
+++ baiduSignInSignUp/js/index.js
@@ -1,6 +1,36 @@
 $(function(){
+	/*********点击登录按钮 蒙板和登录框显示************/
 	$("#login_btn").click(function(){
 		$(".mengban").show();
 		$(".LoginArea").show();
 	});
+	/*******jquery ui 可拖拽************/
+	$(".LoginArea").draggable({handle:".DraggableHeader"});
+	/*********点击登录框中叉叉 蒙板和登录框消失************/
+	$(".closeLoginAbtn").click(function(){
+		$(".mengban").hide();
+		$(".LoginArea").hide();
+	});
+	/*******当登录框中文本框获得焦点*******/
+	$("#LoginUserName").focus(function(){
+		$(".userNameLabel").css("background-position","0 -108px");
+	});
+	$("#LoginPassWord").focus(function(){
+		$(".passwordLabel").css("background-position","0 -187px");
+	});
+
+	/*******当登录框中文本框失去焦点*******/
+	$("#LoginUserName").blur(function(){
+		$(".userNameLabel").css("background-position","");
+	});
+	$("#LoginPassWord").blur(function(){
+		$(".passwordLabel").css("background-position","");
+	});
+
+
+	$("#regaccount").keypress(function(){	
+		
+	});
+	
+
 });
