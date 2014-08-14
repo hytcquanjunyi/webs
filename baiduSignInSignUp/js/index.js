01807a05ccd041efc8b38dc0b81b9f2508beaf0d
hytcquanjunyi <445218372@qq.com>
2014-08-14 09:52:51 +0800

    2014-8-14百度注册登录
    完善了注册登录的提示和验证，更新了索引也的链接

--- baiduSignInSignUp/js/index.js
+++ baiduSignInSignUp/js/index.js
@@ -14,7 +14,9 @@ $(function(){
 	/*******当登录框中文本框获得焦点*******/
 	$("#LoginUserName").focus(function(){
 		$(".userNameLabel").css("background-position","0 -108px");
+
 	});
+
 	$("#LoginPassWord").focus(function(){
 		$(".passwordLabel").css("background-position","0 -187px");
 	});
@@ -28,9 +30,115 @@ $(function(){
 	});
 
 
-	$("#regaccount").keypress(function(){	
+	$("#LoginUserName").keyup(function(){
+		$("#Loginaccountclear").css("opacity","1");
+		
+		if($(this).val().length==0){
+			$("#Loginaccountclear").css("opacity","0");
+		}
+	});
+
+	$("#LoginPassWord").keyup(function(){
+		$("#Loginpasswordclear").css("opacity","1");
+		
+		if($(this).val().length==0){
+			$("#Loginpasswordclear").css("opacity","0");
+		}
+	});
+
+	$(".clearword").click(function(){
+		$(this).prev("input").val("");
+	});
+
+
+
+
+
+
+	$("#regaccount").focus(function(){
+		$("#tips1").show();
+	});
+
+	$("#regaccount").blur(function(){
+		$("#tips1").hide();
+	});
+
+	$("#regaccount").keyup(function(){
+		$("#regaccountclear").css("opacity","1");
+		
+		if($(this).val().length==0){
+			$("#regaccountclear").css("opacity","0");
+		}
+	});
+	
+	$("#regaccountclear").click(function(){
+		$("#regaccount").val("");
+		$("#regaccountclear").css("opacity","0");
+	});
+
+
+	$("#regpassword").focus(function(){
+		$("#tips2").css("visibility","visible");
+	});
+
+	$("#regpassword").blur(function(){
+		if($("#regpassword").val().length<=0){
+			$("#tips2").css("visibility","hidden");
+		}
 		
 	});
+
+	$("#regpassword").keyup(function(){
+
+		$("#regpasswordclear").css("opacity","1");
+
+
+
+		if($(this).val().length>=6 && $(this).val().length<=14){
+			$("#c1").css("background-position","-77px -135px");
+		}
+		else{
+			$("#c1").css("background-position","-77px -150px");
+		}
+		$("#c2").css("background-position","-77px -135px");
+		if($(this).val().length> $.trim($(this).val()).length){
+			$("#c3").css("background-position","-77px -150px");
+		}else{
+			$("#c3").css("background-position","-77px -135px");
+		}  
+		if($(this).val().length==0){
+			$("#regpasswordclear").css("opacity","0");
+			$("#tips2 span").css("background-position", "-77px -115px");
+		} 
+
+	});
+	$("#regpasswordclear").click(function(){
+		$("#regpassword").val("");
+		$("#tips2 span").css("background-position", "-77px -115px");
+		$("#regpasswordclear").css("opacity","0");
+	});
+	
+	$("#regconfrimpassword").keyup(function(){	
+		$("#regconfrimpasswordclear").css("opacity","1");
+		if($(this).val().length==0){
+			$("#regconfrimpasswordclear").css("opacity","0");
+		}
+	});
+	$("#regconfrimpasswordclear").click(function(){
+		$("#regconfrimpassword").val("");
+		$("#regconfrimpasswordclear").css("opacity","0");
+	});
+	
+
+	$("#regconfrimpassword").keyup(function(){
+		if($(this).val()==$("#regpassword").val()){
+			$("#tips3").css("visibility","hidden");
+		}
+		else{
+			$("#tips3").css("visibility","visible");
+		}
+	});
+	
 	
 
 });
