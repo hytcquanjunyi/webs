49a16e05d346b97ca9e7baeae61e28f952ef867d
hytcquanjunyi <445218372@qq.com>
2014-08-15 16:54:25 +0800

    2014-8-15orizon,Tencentweibo
    orizon的一个滚动图，腾讯微博开头

--- orizon/js/index.js
+++ orizon/js/index.js
@@ -1,4 +1,20 @@
 
 $(function(){
-
+	var length=0;
+	 $(".next").click(function(){
+	 		length=length-292;
+	 		if(length<=-876){
+	 			length=0;
+	 			//alert(length);
+	 		}
+	 		$("#hot_news_box").stop().animate({"margin-left":length+'px'},2000);
+	 		
+	 });
+	  $(".prev").click(function(){
+	 		length=length+292;
+	 		if(length>=292){
+	 			length=-584;
+	 		}
+	 		$("#hot_news_box").stop().animate({"margin-left":length+'px'},2000);
+	 });
 });
